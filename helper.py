405f28a52a38c5f0bd49cbc4e8b80d0406b22f1e
Rafael Fantini da Costa <fanta-mnix@users.noreply.github.com>
2017-08-15 21:23:59 -0300

    Answer Question 6

--- helper.py
+++ helper.py
@@ -2,14 +2,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-__all__ = ['transpose', 'pie_chart_for', 'percentualize', 'visualize_distribution_of', 'plot_tsne']
+__all__ = ['transpose',
+           'plot_income_ratio_by',
+           'pie_chart_for',
+           'percentualize',
+           'visualize_distribution_of',
+           'plot_tsne',
+           'most_important']
 
 
 def transpose(nested_list):
     return zip(*nested_list)
 
 
+def plot_income_ratio_by(data, column):
+    assert column in data, "DataFrame does not have a column named '{}'".format(column)
+
+    table = pd.crosstab(data[column], data['income'])
+    col_sum = table.sum(axis=1)
+    high_income_ratio = table.divide(col_sum, axis=0)['>50K']
+
+    plt.figure(figsize=(16, 4))
+    plt.xlabel(column)
+    plt.ylabel('High Income Ratio')
+    plt.xticks(table.index, rotation=90)
+    plt.plot(table.index, high_income_ratio.values)
+    plt.axhline(y=high_income_ratio.values.mean(), c='red')
+    plt.grid()
+    plt.show()
+
+
 def pie_chart_for(data, column):
+    assert column in data, "DataFrame does not have a column named '{}'".format(column)
+
     freqs = transpose(np.unique(data[column], return_counts=True))
     labels, counts = transpose(sorted(freqs, key=lambda x: x[1], reverse=True))
 
@@ -22,17 +47,20 @@ def pie_chart_for(data, column):
 
 
 def percentualize(arr):
+    assert isinstance(arr, np.ndarray), 'Exoected a numpy array'
     return 100 * arr.astype(float) / arr.sum()
 
 
 def visualize_distribution_of(data, column):
+    assert column in data, "DataFrame does not have a column named '{}'".format(column)
+
     plt.title(column)
     plt.hist(data[column])
     plt.tight_layout()
     plt.show()
 
 
-def plot_tsne(plt, X, y=None, perplexity=30.0):
+def plot_tsne(X, y=None, perplexity=30.0):
     from sklearn.manifold.t_sne import TSNE
     import scipy.sparse as sp
 
@@ -57,3 +85,8 @@ def plot_tsne(plt, X, y=None, perplexity=30.0):
     plt.axis('equal')
     plt.tight_layout()
     plt.show()
+
+
+def most_important(learner, feature_names, n):
+    assert len(feature_names) == len(learner.feature_importances_), 'Size mismatch for feature names and importances'
+    return sorted(zip(feature_names, learner.feature_importances_), key=lambda x:x[1], reverse=True)[:n]
